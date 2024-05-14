bb1d9310e0ace763f8fed50eddfb09686260eec6
Himanshu K <himanshu@fuelbuddy.in>
2024-05-14 10:32:48 +0530

    timer complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+function Timer() {
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        if (seconds === 59) {
+          setMinutes((prevMinutes) => prevMinutes + 1);
+          setSeconds(0);
+        } else {
+          setSeconds((prevSeconds) => prevSeconds + 1);
+        }
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  const startTimer = () => {
+    setIsActive(true);
+  };
+
+  const stopTimer = () => {
+    setIsActive(false);
+  };
+
+  const resetTimer = () => {
+    setIsActive(false);
+    setMinutes(0);
+    setSeconds(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="timer">
+      <div className="timer_container">
+        <h1>Timer</h1>
+        <h2>{`${minutes < 10 ? "0" + minutes : minutes}:${
+          seconds < 10 ? "0" + seconds : seconds
+        }`}</h2>
+        <div className="buttons">
+          {!isActive ? (
+            <button className="start" onClick={startTimer}>
+              Start
+            </button>
+          ) : (
+            <button className="stop" onClick={stopTimer}>
+              Stop
+            </button>
+          )}
+          <button className="reset" onClick={resetTimer}>
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Timer;
